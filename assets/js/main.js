84a8fca6986a7475d295096fb12ac2118ddfce11
EvanildoBatista <evanildo.batista@gmail.com>
2025-08-31 22:55:53 -0300

    Feat: Added detail modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type} " onclick="openModal(${pokemon.number})">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -29,6 +29,47 @@ function loadPokemonItens(offset, limit) {
         pokemonList.innerHTML += newHtml
     })
 }
+
+async function openModal(pokemonId) {
+    console.log('Opening modal');
+
+    const pokemonMoreDetail = await pokeApi.getPokemonMoreDetail(pokemonId);
+    const modal = document.getElementById('modal');
+    const modalBody = document.getElementById('modalBody');
+
+    modalBody.innerHTML = `
+        <div class="modalContent ${pokemonMoreDetail.types[0]}">
+            <div class="mainDetailInfo">
+                <h2>${pokemonMoreDetail.name}</h2>
+                <ul class="types">
+                    ${pokemonMoreDetail.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ul>
+                <img src="${pokemonMoreDetail.photo}" alt="${pokemonMoreDetail.name}">
+            </div>
+            <div class="additionalInfo">
+                <span>Abilities<span>
+                <ul class="stats">
+                    ${pokemonMoreDetail.stats.map((stat) => `
+                        <li class="stat">
+                            <span class="name">${stat.name}</span>: <span class="value">${stat.base}</span>
+                        </li>
+                    `).join('')}
+                </ul>
+            </div>
+        </div>
+    `;
+
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+
+const closeModalButton = document.getElementById('closeModal');
+
+closeModalButton.addEventListener('click', closeModal);
 
 loadPokemonItens(offset, limit)
 
---
+++ assets/js/pokemon-more-detail-model.js
@@ -0,0 +1,10 @@
+class PokemonMoreDetail {
+    constructor(id, name, types, photo, abilities, stats) {
+        this.id = id;
+        this.name = name;
+        this.types = types;
+        this.photo = photo;
+        this.abilities = abilities;
+        this.stats = stats;
+    }
+}
